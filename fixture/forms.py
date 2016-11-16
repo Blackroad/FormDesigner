594c27e0d7fcd45fbe9a9963f5ca9fe31ddd4921
Blackroad <VladVell@gmail.com>
2016-11-16 17:55:12 +0200

    Test 1 commit 1

--- fixture/forms.py
+++ fixture/forms.py
@@ -5,16 +5,16 @@ class FormsHelper:
 
     def insert_new_form_page(self):
         wd=self.app.wd
-        if self.is_avaialble("//div/div[@class='btn-group split-button pull-right form-page-change-control']/button"):
-            self.select_previous_page()
-            wd.find_element_by_xpath("//div/div[@class='btn-group split-button pull-right form-page-change-control']/button[text()='Insert Page']").click()
+        wd.implicitly_wait(5)
+        if self.is_avaialble("//div/div[@class='btn-group split-button pull-right form-page-change-control']/button[text()='Insert Page']"):
+            self.select_page_for_editing('2')
+        wd.find_element_by_xpath("//div/div[@class='btn-group split-button pull-right form-page-change-control']/button[text()='Insert Page']").click()
 
     def is_avaialble(self,element_xpath):
         wd = self.app.wd
-        try:
-            element_availability = wd.find_element_by_xpath("%s" % element_xpath).get_attribute('disabled')
-            return element_availability
-        except:
+        if wd.find_element_by_xpath("%s" % element_xpath).get_attribute('disabled'):
+            return True
+        else:
             return False
 
     def select_next_page(self):
@@ -29,21 +29,28 @@ class FormsHelper:
         wd = self.app.wd
         wd.find_element_by_xpath("//form-pagination//button//span[@class='glyphicon glyphicon-step-forward']").click()
 
+    def select_page_for_editing(self,page_number):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@class='btn-group']//button[@class='btn dropdown-toggle btn-default']/span[@class='caret']").click()
+        wd.find_element_by_xpath("//div[@class='dropdown-menu']/ul[@class='dropdown-menu']/li[@ng-click='ddeCtrl.select(option)']/span[text()='%s']" % page_number).click()
 
     def delete_form_page(self):
         wd = self.app.wd
         wd.find_element_by_xpath("//div[@class='btn-group split-button pull-right form-page-change-control']//button[@class='btn btn-default dropdown-toggle']//span[@class='caret']").click()
         if not self.is_avaialble("//div[@class='btn-group split-button pull-right form-page-change-control open']//li"):
             wd.find_element_by_xpath("//div[@class='btn-group split-button pull-right form-page-change-control open']//li//a[text()='Delete Page']").click()
-            self.wait_for_allert_window("")
+            wd.switch_to_alert()
+            try:
+                wd.find_element_by_xpath("//div[@class='modal-content']//button[@ng-repeat='button in popUpContentCtrl.buttons']").click()
+            except:
+                return False
 
 
-
-    def allert_for_delete_form(self,Delete=None):
+    def wait(self,main_page_element):
         wd = self.app.wd
-        if Delete == True:
-            wd.find_element_by_xpath("//div//button[@class='btn btn-default ng-binding ng-scope' and text()='%s']"%Delete).click()
-        wd.find_element_by_xpath("//div//button[@class='btn btn-default ng-binding ng-scope' and text()='Cancel']").click()
+        while len(wd.find_elements_by_xpath("%s']" % main_page_element)) > 0:
+            pass
+        wd.implicitly_wait(5)
+        return
 
-    def wait_for_allert_window(self, allert_widnow):
-        
+
